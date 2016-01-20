252364887c47685e05249bbfd15c5353808bff9e
Kirk <kjsstennett@gmail.com>
2016-01-19 17:51:51 -0800

    checking

--- views/cprm/home.php
+++ views/cprm/home.php
@@ -1,7 +1,7 @@
-<div class="container">
+<div class="container-fluid">
 	<div class="panel panel-default" style="margin-top:5px;">
-		<div class="panel-heading" style="height:20px;">
-			<div class="col-md-2 col-md-offset-1">
+		<div class="panel-heading" style="height:43px;">
+			<!-- <div class="col-md-2 col-md-offset-1">
 				<p class="panel-nav text-center">Courses</p>
 			</div>
 			<div class="col-md-2">
@@ -15,7 +15,37 @@
 			</div>
 			<div class="col-md-2">
 				<p class="panel-nav text-center">Student Information</p>
+			</div> -->
+			<!-- <div class="panel-navbar">
+				<ul class="list-inline">
+					<div class="col-md-2 col-md-offset-1">
+						<li class="text-center">Courses</li>
+					</div>
+					<div class="col-md-2">
+						<li class="text-center">Peer Reviews</li>
+					</div>
+					<div class="col-md-2">
+						<li class="text-center">Grades</li>
+					</div>
+					<div class="col-md-2">
+						<li class="text-center">Feedback Submission</li>
+					</div>
+					<div class="col-md-2">
+						<li class="text-center">Student Information</li>
+					</div>
+					
+				</ul>
+			</div> -->
+			<div class="panel-navbar">
+				<ul>
+					<li>Courses</li>
+					<li>Peer Reviews</li>
+					<li>Grades</li>
+					<li>Feedback Submission</li>
+					<li>Student Information</li>
+				</ul>
 			</div>
+			
 		</div>
 		
 		<div class="panel-body">
