15175867df607e47821f9d341d84d3ef8d5f5532
Tony Anziano <tonyanziano5@gmail.com>
2016-01-25 22:41:17 -0800

    Testing AJAX pt3

--- views/cprm/home.php
+++ views/cprm/home.php
@@ -2,7 +2,7 @@
 	<div class="panel panel-default" style="margin-top:5px;">
 		<div class="panel-heading panel-head-color" style="height:49px;">
 			
-			
+			<!--
 			<div class="panel-navbar">
 				<ul>
 					<li><span class="nav-span">Courses</span></li>
@@ -12,9 +12,27 @@
 					<li><span class="nav-span">Student Information</span></li>
 				</ul>
 			</div>
+			-->
 			
-			
+		
 			<!-- ### -->
+			<div class="panel-navbar">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#website-navbar" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Brand</a>
+			</div>
+			
+			<div class="collaps navbar-collapse" id="website-navbar">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">Grades</a></li>
+				</ul>
+			</div>
+			
 			
 		</div>
 		
