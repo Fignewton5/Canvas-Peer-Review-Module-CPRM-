de5f9ade0eb8e61eda8bf964ee5c65b1c09d46f3
Kirk <kjsstennett@gmail.com>
2016-01-26 23:07:46 -0800

    feedback change basic layout and css implementations

--- views/cprm/feedback.php
+++ views/cprm/feedback.php
@@ -1,3 +1,38 @@
-<div class="container-fluid">
-	Feedback Page!!!
+<div class="container-fluid remove-paddingLR">
+	<div class="row" style="margin-top:15px;">
+		<div class="col-md-4" style="float:left;">
+			<div class="feedback-side-menu">
+				<div style="padding-bottom:2px;">
+					<button type="button" class="btn btn-default feedback-button-fixes" id="single">
+						Single Reviews
+					</button>
+				</div>
+				<div style="padding-top:2px;">
+					<button type="button" class="btn btn-default feedback-button-fixes" id="group">
+						Group Reviews
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-8" style="float:left;width:80%;">
+			<div style="width:100%;">
+				<table class="table table-striped table-condensed">
+					<thead>
+						<tr>
+							<th>Evaluation Criteria</th>
+							<th>Points Possible</th>
+							<th>Points Earned</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="feedback-tb-top-padding">Sample Presentation</td>
+							<td class="feedback-tb-top-padding">5 Points</td>
+							<td><input type='text' style="width:20%;" class='form-control' /></td>	
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
