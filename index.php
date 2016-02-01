eec3cad8be3b3bf96cbf67b0861d0f3bb5f362c6
Kirk <kjsstennett@gmail.com>
2016-01-31 17:24:33 -0800

    more testing

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	$mysqlCon = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($mysqlCon));
 	mysqli_select_db($mysqlCon, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error($mysqlCon));
 	mysqli_query($mysqlCon, "INSERT INTO Test (review,maxPoints,field1) VALUES ('cs462','5','performance')");
+	$result = mysqli_query($mysqlCon, "SELECT * FROM Test");
+	var_dump($result);
 	if (isset($_GET['controller']) && isset($_GET['action'])) {
 		$controller = $_GET['controller'];
 		$action 	= $_GET['action'];
