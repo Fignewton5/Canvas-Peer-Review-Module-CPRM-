f5b318ecf0d665069b4dfc64ae9d7cfcdef9a8fe
Kirk <kjsstennett@gmail.com>
2016-01-31 17:52:10 -0800

    minor fixes

--- connection.php
+++ connection.php
@@ -8,8 +8,11 @@
 		
 		public static function getInstance() {
 			if (self::$instance == NULL) {
-				self::$instance = new self(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error(self::$instance));
-				mysqli_select_db(self::$instance, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error(self::$instance));
+				self::$instance = new self(getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+					getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
+					getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), 
+					getenv('OPENSHIFT_APP_NAME'), 
+					getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error(self::$instance));
 			}
 			
 			return self::$instance;
