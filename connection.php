ab35b94c512f5c324ac14a5d18832a03e7c60bc8
Kirk <kjsstennett@gmail.com>
2016-02-01 23:33:29 -0800

    minor DB fixes

--- connection.php
+++ connection.php
@@ -6,13 +6,13 @@
 		//gbx6usvfglgc
 		private static $instance = null;
 		
-		private function __construct() {
-			
-		}
+		private function __construct($host, $user, $password, $database, $port){ 
+        	parent::__construct($host, $user, $password, $database, $port);
+     	}
 		
 		public static function getInstance() {
 			if (self::$instance == null) {
-				self::$instance = new self();
+				self::$instance = new self("127.6.241.130", "admint7unJmx", "gbx6usvfglgc", "cprmphp", "3306");
 				
 			}
 			
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 	//using requiremind's PHP MVC format - http://requiremind.com/a-most-simple-php-mvc-beginners-tutorial/
-	echo getenv('OPENSHIFT_MYSQL_DB_HOST');
 	require_once('connection.php');
 	if (isset($_GET['controller']) && isset($_GET['action'])) {
 		$controller = $_GET['controller'];
