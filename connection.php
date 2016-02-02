082ca205efa99a22e296e2a0c32a335eaea60f0d
Kirk <kjsstennett@gmail.com>
2016-02-01 22:37:30 -0800

    modified layout of db info entry for readability

--- connection.php
+++ connection.php
@@ -3,12 +3,18 @@
 		private static $instance = NULL;
 		
 		private function __construct($host, $user, $pw, $db, $port) {
+			echo "WORKING";
 			parent::__construct($host, $user, $pw, $db, $port);
 		}
 		
 		public static function getInstance() {
 			if (self::$instance == NULL) {
-				self::$instance = new self(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), getenv('OPENSHIFT_APP_NAME'), getenv('OPENSHIFT_MYSQL_DB_PORT'));
+				define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+				define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+				define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+				define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+				define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+				self::$instance = new self(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 			}
 			
 			return self::$instance;
