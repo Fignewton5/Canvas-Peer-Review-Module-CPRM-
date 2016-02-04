b43b6d9c4b5979db08df57617d28749fa0b3d587
Kirk <kjsstennett@gmail.com>
2016-02-03 19:21:56 -0800

    adding data from database into feedback

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	//using requiremind's PHP MVC format - http://requiremind.com/a-most-simple-php-mvc-beginners-tutorial/
-	//require('connection.php');
+	require_once('connection.php');
 	if (isset($_GET['controller']) && isset($_GET['action'])) {
 		$controller = $_GET['controller'];
 		$action 	= $_GET['action'];
--- views/cprm/feedback.php
+++ views/cprm/feedback.php
@@ -1,8 +1,8 @@
-<div class="container-fluid remove-paddingLR">
+<div class="container-fluid">
 	<div class="panel panel-default" style="margin-top:5px;">
 		<div class="panel-body" id="test">
 			<div class="row" style="margin-top:15px;">
-				<div class="col-md-4" style="float:left;">
+				<div class="col-md-3" style="float:left;">
 					<div class="feedback-side-menu">
 						<div style="padding-bottom:2px;">
 							<button type="button" class="btn btn-default feedback-button-fixes" id="single">
@@ -17,38 +17,40 @@
 					</div>
 				</div>
 				<?php
-				//relative because local hated it...
-				require_once('connection.php');
+				
+				
 				$db = Db::getInstance();
 				$query = 'SELECT * FROM Test WHERE reviewName="cs462"';
 				foreach ($db->query($query) as $row) {
-					echo $row['reviewName'] . " " . $row['pointMax'] . $row['field1'] . $row['pointFor1'];
-				}
-				// $result = $db->query("SELECT * FROM Test WHERE reviewName='cs462'");
-				// while ($row = mysqli_fetch_array($result)) {
-					// echo $row['reviewName'] . " " . $row['pointMax'] . $row['field1'] . $row['pointFor1'];
-				// }
+					//echo $row['reviewName'] . " " . $row['pointMax'] . $row['field1'] . $row['pointFor1'];
+				
 				?>
-				<div class="col-md-8" style="float:left;">
-					<div style="width:100%;">
-						<table class="table table-striped table-condensed">
-							<thead>
-								<tr>
-									<th>Evaluation Criteria</th>
-									<th>Points Possible</th>
-									<th>Points Earned</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td class="feedback-tb-top-padding">Sample Presentation</td>
-									<td class="feedback-tb-top-padding">5 Points</td>
-									<td><input type='text' style="width:20%;" class='form-control' /></td>	
-								</tr>
-							</tbody>
-						</table>
+				<form action="" method="post">
+					<div class="col-md-7" style="float:left;">
+						<div style="width:100%;">
+							<table class="table table-striped table-condensed">
+								<thead>
+									<tr>
+										<th>Evaluation Criteria</th>
+										<th>Points Possible</th>
+										<th>Points Earned</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td class="feedback-tb-top-padding"><?php echo $row['field1']; ?></td>
+										<td class="feedback-tb-top-padding"><?php echo $row['pointMax'];?> Points</td>
+										<td><input type='text' style="width:20%;" class='form-control' value='<?php echo $row['pointFor1'];?>' /></td>	
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
-				</div>
+					<?php } ?>
+					<div class="col-md-2">
+						<button type='submit' class='btn btn-default' />
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
