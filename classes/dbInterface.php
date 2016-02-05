b8509716a00d876cbdb47371ff8e1db062e04798
Kirk <kjsstennett@gmail.com>
2016-02-04 17:39:02 -0800

    query checks and such

--- classes/dbInterface.php
+++ classes/dbInterface.php
@@ -13,8 +13,11 @@
 			require_once('classes/canvasWrapper.php');
 			$canvas = new CanvasWrapper();
 			$user = $canvas->formatUserData();
-			$sql = "INSERT INTO users (osuId,token,name) VALUES ('" . $user->user_id . "','" . $_SESSION['token'] . "'," . $user->name . "')";
-			$this->db->query($sql);
+			$sql = "INSERT INTO users (osuId,token,name) VALUES ('" . $user->user_id . "','" . $token . "','" . $user->name . "')";
+			$result = $this->db->query($sql);
+			if ($result == FALSE) {
+				echo "QUERY FAILED";
+			}
 		}
 		
 		//checks user token to see if it exists returns bool
