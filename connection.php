6eebb6cc7b458bea0cd405233a95eb2b36ed92b4
Kirk <kjsstennett@gmail.com>
2016-02-10 20:17:03 -0800

    cleaning up connection.php, and removed canvasAuth

--- connection.php
+++ connection.php
@@ -1,24 +1,4 @@
 <?php
-	// class Db extends MySQLi {
-		// //admint7unJmx
-		// //127.6.241.130:3306
-		// //cprmphp
-		// //gbx6usvfglgc
-		// private static $instance = null;
-// 		
-		// private function __construct($host, $user, $password, $database, $port){ 
-        	// parent::__construct($host, $user, $password, $database, $port);
-     	// }
-// 		
-		// public static function getInstance() {
-			// if (self::$instance == null) {
-				// self::$instance = new self("127.6.241.130", "admint7unJmx", "gbx6usvfglgc", "cprmphp", "3306");
-// 				
-			// }
-// 			
-			// return self::$instance;
-		// }
-	// }
 	class Db {
 		private static $instance = NULL;
 		
