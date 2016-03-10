022d50caa8dd13b09827c0daa126bb740b6a808b
Tony Anziano <tonyanziano5@gmail.com>
2016-03-09 21:34:08 -0800

    Changed peer reviews tab, and added javascript to create rubric.

--- views/cprm/peerReviews.php
+++ views/cprm/peerReviews.php
@@ -9,42 +9,36 @@
 <div class="container-fluid">
 	<div class="panel panel-default" style="margin-top:5px;">
 		<div class="panel-body" id="test">
+		
+			<?php
+				//rubric submission testing
+				for($i = 0; $i < 10; $i++){
+				
+					if (isset($_POST['row' . $i . 'crit'])){
+						echo '<div>' . $_POST['row' . $i . 'crit'] . '</div>';
+					}
+					if (isset($_POST['row' . $i . 'pts'])){
+						echo '<div>' . $_POST['row' . $i . 'pts'] . '</div>';
+					}
+				}
+			?>
+		
 			<div class="row" style="margin-top:15px;">
 				<div class="col-md-3" style="float:left;">
 					<div class="peerReview-side-menu">
 						<div style="padding-bottom:2px;">
-							<button type="button" class="btn btn-default feedback-button-fixes" id="single" onclick="createRubric()">
-								Create Rubric
-							</button>
+							<button type="button" class="btn btn-default feedback-button-fixes" id="single" onclick="createRubric()">Create Rubric</button>
 						</div>
 						<div style="padding-top:2px;">
-							<button type="button" class="btn btn-default feedback-button-fixes" id="group" onclick="loadRubric()">
-								Load Rubric
-							</button>
+							<button type="button" class="btn btn-default feedback-button-fixes" id="group" onclick="assignGroup()">Assign Group</button>
 						</div>
-						<div style="padding-top:2px;">
-							<button type="button" class="btn btn-default feedback-button-fixes" id="group" onclick="assignGroup()">
-								Assign Group
-							</button>
-						</div>
-						<div style="padding-top:2px;">
-							<button type='submit' class='btn btn-default'>Submit</button>
-						</div>
-					</div>
-				</div>
-				<?php
+					</div> <!-- peerReview-side-menu -->
+				</div> <!-- col-md-3 -->
 				
-				$db = Db::getInstance();
-				$query = 'SELECT * FROM Test1 WHERE reviewName="cs462"';
-
-				foreach ($db->query($query) as $row) {
-					//only pulls one row
-					$iter = $row['fieldsUsed'];
-				
-				?>
-				<form action="" method="post">
+				<!-- this will be the rubric table -->
+				<form action="?controller=cprm&action=peerReviews" method="post" id="rubric-table">
 					<div class="col-md-7" style="float:left;">
-						<div style="width:100%;">
+						<div style="width:100%;" id="rubric-container">
 							<table class="table table-striped table-condensed">
 								<thead>
 									<tr>
@@ -52,44 +46,94 @@
 										<th>Points Possible</th>
 									</tr>
 								</thead>
-								<tbody>
-									<?php
-										for ($i = 0; $i < $iter; $i++) {
-											echo "<tr>";
-											//echo "<td class='feedback-tb-top-padding'>" . $row['field' . ($i + 1)] . "</td>";
-											echo "<td><input type='text' class ='form-control' value='" . $row['field' . ($i + 1)] . "'></td>";
-											echo "<td><input type='text' style='width:20%' class='form-control' value='" . $row['pMax' . ($i + 1)] . "' /></td>";
-											echo "</tr>";
-										}
-										
-									?>
+								
+								<!-- rubric content goes here (criteria) 
+										require at least one row so that
+										an empty form can't be submitted
+								-->
+								<tbody id="rubric-table-body">
+									<tr>
+										<td><input type="text" class="form-control" value="" name="row0crit" required></td>
+										<td><input type="text" class="form-control" value="" name="row0pts" style="width:20%" required></td>
+									</tr>
 								</tbody>
-							</table>
-						</div>
-					</div>
-					<?php } ?>
+							</table> <!-- rubric table -->
+						</div> <!-- div (rubric-container) -->
+						
+						<!-- Buttons that will add or remove rows from the rubric -->
+						<div class="col-md-12">
+							<div class="col-md-5">
+								<button type='button' class='btn btn-default' onclick="addRowRubric()">Add Row</button>
+								<button type='button' class='btn btn-default' onclick="removeRowRubric()">Remove Row</button>
+							</div>
+							<div class="col-md-5">
+								<button type="submit" class="btn btn-default">Submit Rubric</button>
+							</div>
+						</div> <!-- button row -->
+						
+					</div> <!-- col-md-7 -->
+				</form> <!-- rubric form -->
 				
-					<button type='button' class='btn btn-default' onclick="addRowRubric()">Add Row</button>
-				</form>
-			</div>
-		</div>
-	</div>
-</div>
+				<!-- this will be the group assign UI -->
+				<div class="cold-md-7" id="group-assign-table" style="display:none">
+					GROUP ASSIGN
+				<div>
+				
+			</div> <!-- row -->
+		</div> <!-- panel-body -->
+	</div> <!-- panel panel-default -->
+</div> <!-- container-fluid -->
 
 <script type="text/javascript">
+
+	//adds a criteria row to the rubric creation table
 	function addRowRubric(){
-		alert("Add Row button has been clicked!");
+	
+		//first count how many rows are inside the rubric table
+		//because our database only allows for 10 criteria per rubric
+		var rowCount = $("#rubric-table-body > tr").length;
+		
+		//if less than 10 rows, add a new row
+		if(rowCount < 10){
+		
+			//create <tr></tr> element
+			var row = document.createElement("tr");
+			//fill element with table row data
+			var td1 = '<td><input type="text" class="form-control" value="" name="row' + rowCount + 'crit" required></td>';
+			var td2 = '<td><input type="text" style="width:20%" class="form-control" value=""  name="row' + rowCount + 'pts" required></td>';
+			var rowData = td1 + td2;
+			row.innerHTML = rowData;
+			
+			//plug the row into the <tbody> 
+			$("#rubric-table-body").append(row);
+		}
+		else alert("Maximum amount of rows reached! [10]");
 	}
 	
+	//removes a criteria row from the rubric creation table
+	function removeRowRubric(){
+	
+		//first count how many rows are inside the rubric table
+		//so that we don't try to remove a row when we have 1 row
+		var rowCount = $("#rubric-table-body > tr").length;
+		
+		//if more than 1 row, remove a row
+		if(rowCount > 1){
+			//remove the last row in the rubric table
+			$("#rubric-table-body > tr:last").remove();
+		}
+		else alert("Can't remove first row!");
+	}
+	
+	//displays the HTML for creating a rubric
 	function createRubric(){
-		alert("Create Rubric button has been clicked!");
+		$("#group-assign-table").hide();
+		$("#rubric-table").show();
 	}
 	
-	function loadRubric(){
-		alert("Load Rubric button has been clicked!");
-	}
-	
+	//displays the HTML for assigning a group
 	function assignGroup(){
-		alert("Assign Group button has been clicked!");
+		$("#rubric-table").hide();
+		$("#group-assign-table").show();
 	}
 </script>
